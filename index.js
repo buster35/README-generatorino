21b13be5124f9a88dc08834142b67c8a28c2e252
Sleepy Cat <gilb2966@gmail.com>
2023-04-21 19:40:12 -0500

    finished building question prompt arrays

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 1. build directory
 2. npm init and install dependencies
 3. requires in index.js
-4. TODO:skeleton for markdown
+4. skeleton for markdown
 5. TODO:prompt questions *test
 6. TODO:build functions
 7. TODO:connect markdown with index.js
@@ -11,26 +11,113 @@
 10.
 */
 
-
-const inquirer = require("inquirer")
-const fs = require("fs")
+const inquirer = require("inquirer");
+const fs = require("fs");
 //i'll include the functions contained in generateMarkdown.js here, which will cause them to fire once the app is initialized//
-const {} = require("./utils/generateMarkdown")
+const {} = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [{
-  
-}];
+inquirer.prompt(title, description, instructions, collaborators, tests, license).then((response) => {console.log(response.name)})
+
+//variables containing question hashes//
+const title = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "projectTitle",
+  },
+];
+
+const description = [
+  {
+    type: "input",
+    message: "What was your motivation?",
+    name: "motivation",
+  },
+  {
+    type: "input",
+    message: "Why did you build this project?",
+    name: "whyBuild",
+  },
+  {
+    type: "input",
+    message: "What problem does it solve?",
+    name: "problemsSolved",
+  },
+  {
+    type: "input",
+    message: "What did you learn?",
+    name: "whatWasLearned",
+  },
+];
+
+const instructions = [
+  {
+    type: "input",
+    message: "What instructions are required for the user to use your project?",
+    name: "instructions",
+  },
+  {
+    type: "confirm",
+    message: "Do you want to add a screenshot?",
+    name: "screenshot1",
+    when: gfsd, //ternary statement?
+  },
+  {
+    type: "confirm",
+    message: "Do you have any examples for use of the program?",
+    name: "screenshot2",
+    when: fdsa,
+    //ternary statement?
+  },
+];
+
+const collaborators = [
+  {
+    type: "input",
+    message: "What collaborators were involved on this project?",
+    name: "collaborators",
+  },
+  {
+    type: "input",
+    message: "What are these collaborators' github url?",
+    name: "collaboratorsGithub",
+  },
+  {
+    type: "input",
+    message: "What third-party assets were used?",
+    name: "thirdPartyAssets",
+  },
+  {
+    type: "input",
+    message: "What tutorials were used to design this project?",
+    name: "tutorials",
+  },
+];
+
+const tests = [
+  {
+    type: "input",
+    message: "What tests were run for this project?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "How can the user run them if they so choose?",
+    name: "testRun",
+  },
+];
+
+const license = [
+  {
+    type: "list",
+    message: "What license are you using for your project?",
+    name: "license",
+    choices: ["MIT", "jdfkslalkdasf"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
-
-
-
-
-
-
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -38,40 +125,15 @@ function init() {}
 // Function call to initialize app
 init();
 
-
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-`# ${projectTitle}`
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+`# ${projectTitle}`// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-`## ${repoDescription} "\n"`
-// What was your motivation?
-// Why did you build this project?
-// What problem does it solve?
-// What did you learn?
-`## ${tableOfContents} "\n"`
-`## ${installationInstructions} "\n"`
-// What are the steps required to install your project?
-`## ${usageInformation} "\n"`
-// What instructions are required for the user to use your project?
-// Do you want to add a screenshot?
-// Do you have any examples for use of the program?
-`## ${contributionGuidelines} "\n"`
-// What collaborators were involved on this project?
-// What are these collaborators' github url?
-// What third-party assets were used?
-// What tutorials were used to design this project?
-`## ${testInstructions} "\n"`
-// What tests were run for this project?
-// How can the user run them if they so choose?
-`## ${license} "\n"`
-// ![GitHub](https://img.shields.io/github/license/buster35/README-generatorino) **Badge icon**
-// What license did you use for your project? (https://choosealicense.com/) 
+`## ${repoDescription} "\n"``## ${tableOfContents} "\n"``## ${installationInstructions} "\n"``## ${usageInformation} "\n"``## ${contributionGuidelines} "\n"`//    https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+`## ${testInstructions} "\n"``## ${license} "\n"`// ![GitHub](https://img.shields.io/github/license/buster35/README-generatorino) **Badge icon**
+//  (https://choosealicense.com/)
+
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-`## ${questions} "\n" ${github} "\n" ${email} "\n"`
+`## ${questions} "\n" ${github} "\n" ${email} "\n"`;
 
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
