fca0f0af02494917b3a06a93fdfb769341d47cf7
Sleepy Cat <gilb2966@gmail.com>
2023-04-24 09:37:35 -0500

    fixed file variable to allow anchor links to jump to correct sections

--- gen.js
+++ gen.js
@@ -13,16 +13,16 @@ function generateMarkdown(data) {
   let licenseLink = data.license
     switch (licenseBadge) {
       case "MIT":
-        licenseBadge = `MIT![GitHub](https://img.shields.io/github/license/${data.userGithub}/README-generatorino)`;
+        licenseBadge = `![GitHub](https://img.shields.io/github/license/${data.userGithub}/README-generatorino)`;
         break;
       case "GNU GPLv3": 
-        licenseBadge = `GNU GPLv3![GitHub](https://img.shields.io/github/license/${data.userGithub}/README-generatorino)`;
+        licenseBadge = `![GitHub](https://img.shields.io/github/license/${data.userGithub}/README-generatorino)`;
         break;
       default: "None";
         break;
     }
 
-  let file = `# ${data.projectTitle}\n## Description\n${data.motivation}\n${data.whyBuild}\n${data.problemsSolved}\n${data.whatWasLearned}\n## Table of Contents:\n### [Instructions](/README.md/#instructions-instructions)\n### [Screenshots](/README.md/#screenshots-screenshots)\n### [Contributions](/README.md/#contributions-contributions)\n### [Tests](/README.md/#tests-tests)\n### [License](/README.md/#license-license)\n## Instructions{#instructions}\n${data.instructions}\n## Screenshots{#screenshots}\n${data.screenshot1}\n## Contributions{#contributions}\n${data.collaborators}\n${data.collaboratorsGithub}\n${data.thirdPartyAssets}\n${data.tutorials}\n## Tests{#tests}\n${data.tests}\n${data.testRun}\n## Questions{#questions}\nPlease direct any further questions to: ${data.userGithub} or ${data.email}\n## License{#license}\nThe license used for this project is ${licenseBadge}.\n`
+  let file = `# ${data.projectTitle}${licenseBadge}\n## Description\n${data.motivation}\n${data.whyBuild}\n${data.problemsSolved}\n${data.whatWasLearned}\n## Table of Contents:\n### [Instructions](#instructions)\n### [Screenshots](#screenshots)\n### [Contributions](#contributions)\n### [Tests](#tests)\n### [License](#license)\n## Instructions\n${data.instructions}\n## Screenshots\n${data.screenshot1}\n## Contributions\n${data.collaborators}\n${data.collaboratorsGithub}\n${data.thirdPartyAssets}\n${data.tutorials}\n## Tests\n${data.tests}\n${data.testRun}\n## Questions\nPlease direct any further questions to: ${data.userGithub} or ${data.email}\n## License\nThe license used for this project is ${data.license}.\n`
 
   writeToFile(fileName, file)
 }
